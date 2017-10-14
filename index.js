a69b3019b5a6d60ca1d0131307c5132bb95e49ea
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-14 21:54:36 +0700

    Query & Webpack config fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./server/server');
+const app = require("./server/server");
 
-app.listen(4000, () => {
-  console.log('Listening');
+app.listen(5000, () => {
+  console.log("Listening");
 });
